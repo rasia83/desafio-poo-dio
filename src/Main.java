fc52a53bc7b86f025e7a53483b4117e9a1e57254
rasia83 <tiago_rasia@hotmail.com>
2023-03-20 10:29:59 -0300

    Herança e polimorfismo, parte 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String args[]) {
-        Curso curso1 = new Curso();
+        Conteudo curso1 = new Curso();
         curso1.setTitulo("Java");
-        curso1.setDescricao("POO com java");
-        curso1.setCargaHoraria(4);
-        System.out.println(curso1);
+        curso1.setDescricao("POO com Java");
+        ((Curso)curso1).setCargaHoraria(4);
 
-        Mentoria m = new Mentoria();
+        Conteudo mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Mentoria POO com Java");
+        ((Mentoria)mentoria1).setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
+
     }
 
 }
